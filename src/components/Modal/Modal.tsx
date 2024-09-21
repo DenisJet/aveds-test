5853314f46984854d995bea355df6e5f09738b10
Denis Kalkopf <ddenproton@protonmail.com>
2024-09-21 22:20:52 +0500

    add form

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,9 +1,18 @@
-import { useContext } from 'react';
+import { FormEvent, useContext } from 'react';
 import styles from './Modal.module.css';
 import { ModalContext } from '../../context/modalContext';
+import Button from '../Button/Button';
 
 export default function Modal(): JSX.Element {
   const { isModalOpen, setIsModalOpen } = useContext(ModalContext);
+
+  const submit = async (e: FormEvent) => {
+    e.preventDefault();
+    // dispatch(userActions.clearLoginError());
+    // const target = e.target as typeof e.target & LoginForm;
+    // const { email, password } = target;
+    // await sendLogin(email.value, password.value);
+  };
 
   if (isModalOpen) {
     return (
@@ -15,7 +24,18 @@ export default function Modal(): JSX.Element {
           className={isModalOpen ? `${styles.modalContent} ${styles.active}` : styles.modalContent}
           onClick={(evt) => evt.stopPropagation()}
         >
-          Modal
+          <h4 className={styles.modalTitle}>Вход</h4>
+          <form className={styles.modalForm} onSubmit={submit}>
+            <div className={styles.modalFormField}>
+              <label htmlFor='login'>Ваш логин</label>
+              <input id='login' name='login' type='text' placeholder='Логин' />
+            </div>
+            <div className={styles.modalFormField}>
+              <label htmlFor='password'>Ваш пароль</label>
+              <input id='password' name='password' type='password' placeholder='Пароль' />
+            </div>
+            <Button>Войти</Button>
+          </form>
         </div>
       </div>
     );
