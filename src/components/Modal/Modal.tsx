d9716f624ab00111cd04cb5e2c0a7d27713d50d4
Denis Kalkopf <ddenproton@protonmail.com>
2024-09-21 23:46:28 +0500

    fix

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,17 +1,70 @@
-import { FormEvent, useContext } from 'react';
+import { FormEvent, useContext, useState } from 'react';
 import styles from './Modal.module.css';
 import { ModalContext } from '../../context/modalContext';
 import Button from '../Button/Button';
 
+export type LoginForm = {
+  login: {
+    value: string;
+  };
+  password: {
+    value: string;
+  };
+};
+
 export default function Modal(): JSX.Element {
   const { isModalOpen, setIsModalOpen } = useContext(ModalContext);
 
+  const [login, setLogin] = useState('');
+  const [password, setPassword] = useState('');
+  const [loginDirty, setLoginDirty] = useState(false);
+  const [passwordDirty, setPasswordDirty] = useState(false);
+  const [loginError, setLoginError] = useState(false);
+  const [passwordError, setPasswordError] = useState(false);
+  const [passwordType, setPasswordType] = useState('password');
+
+  const [isTrueCredentials, setIsTruCredentials] = useState(true);
+
+  const loginHandler = (e: FormEvent<HTMLInputElement>) => {
+    setLogin(e.currentTarget.value);
+    if (e.currentTarget.value.length < 3) {
+      setLoginError(true);
+    } else {
+      setLoginError(false);
+    }
+  };
+
+  const passwordHandler = (e: FormEvent<HTMLInputElement>) => {
+    setPassword(e.currentTarget.value);
+    if (e.currentTarget.value.length < 8) {
+      setPasswordError(true);
+    } else {
+      setPasswordError(false);
+    }
+  };
+
+  const blurHandler = (e: FormEvent<HTMLInputElement>) => {
+    switch (e.currentTarget.name) {
+      case 'login':
+        setLoginDirty(true);
+        break;
+      case 'password':
+        setPasswordDirty(true);
+        break;
+    }
+  };
+
+  const passwordTypeChange = () => {
+    if (passwordType === 'password') {
+      setPasswordType('text');
+    } else {
+      setPasswordType('password');
+    }
+  };
+
   const submit = async (e: FormEvent) => {
     e.preventDefault();
-    // dispatch(userActions.clearLoginError());
-    // const target = e.target as typeof e.target & LoginForm;
-    // const { email, password } = target;
-    // await sendLogin(email.value, password.value);
+    console.log(login, password);
   };
 
   if (isModalOpen) {
@@ -28,13 +81,37 @@ export default function Modal(): JSX.Element {
           <form className={styles.modalForm} onSubmit={submit}>
             <div className={styles.modalFormField}>
               <label htmlFor='login'>Ваш логин</label>
-              <input id='login' name='login' type='text' placeholder='Логин' />
+              <input
+                className={loginError ? styles.inputError : ''}
+                onChange={(e) => loginHandler(e)}
+                onBlur={(e) => blurHandler(e)}
+                id='login'
+                name='login'
+                type='text'
+                placeholder='Логин'
+              />
+              {loginDirty && loginError && <small className={styles.error}>Минимум 3 символа</small>}
             </div>
             <div className={styles.modalFormField}>
               <label htmlFor='password'>Ваш пароль</label>
-              <input id='password' name='password' type='password' placeholder='Пароль' />
+              <input
+                className={loginError ? styles.inputError : ''}
+                onChange={(e) => passwordHandler(e)}
+                onBlur={(e) => blurHandler(e)}
+                id='password'
+                name='password'
+                type={passwordType}
+                placeholder='Пароль'
+              />
+              <span className={styles.hide} onClick={passwordTypeChange}>
+                <img src='/hide.svg' alt='показать' width={24} height={24} />
+              </span>
+              {passwordDirty && passwordError && <small className={styles.error}>Минимум 8 символов</small>}
             </div>
-            <Button>Войти</Button>
+            <div>
+              {!isTrueCredentials && <span className={styles.submitError}>Неверный логин или пароль</span>}
+              <Button type='submit'>Войти</Button>
+            </div>
           </form>
         </div>
       </div>
